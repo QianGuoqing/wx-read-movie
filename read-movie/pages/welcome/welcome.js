a74b78412840a975183bc5285a40de2810c26a7f
QianGuoqing <qian_guoqing@yeah.net>
2018-05-22 16:34:14 +0800

    配置基础设置，完成welcome页面

---
+++ read-movie/pages/welcome/welcome.js
@@ -0,0 +1,28 @@
+// pages/welcome/welcome.js
+const days = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    welcome: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      welcome: this._getDate()
+    })
+  },
+  _getDate() {
+    let now = new Date()
+    let year = now.getFullYear()
+    let month = now.getMonth() + 1
+    let date = now.getDate()
+    let day = now.getDay()
+    return `${year}年${month}月${date}日 ${days[day]}`
+  }
+})
